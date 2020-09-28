6bfb858c01392d53f012ef760bd95673bafbbd08
carla <cfpcarla@gmail.com>
2020-09-28 16:54:51 -0400

    Finished timeline from about component

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -3,6 +3,7 @@ import React from "react";
 // import CarlaMedeiros from "../assets/photo/CarlaMedeiros.JPG";
 import about from "../assets/about.png";
 import carla from "../assets/carla.jpg";
+import SchoolIcon from "@material-ui/icons/School";
 
 import {
   Box,
@@ -27,6 +28,7 @@ import HotelIcon from "@material-ui/icons/Hotel";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import Paper from "@material-ui/core/Paper";
 import CodeIcon from "@material-ui/icons/Code";
+import GavelIcon from "@material-ui/icons/Gavel";
 
 // import DeveloperCard from "./side-components/developer-card";
 const AboutStyle = makeStyles((theme) => ({
@@ -71,26 +73,71 @@ const About = () => {
           </Typography>
         </Grid>
 
-        <Grid item xs={12} md={8} className={classes.content}>
+        <Grid item xs={12} md={8}>
           <Timeline align="alternate">
             <TimelineItem>
               <TimelineSeparator>
                 <TimelineDot>
-                  <LaptopMacIcon />
+                  <GavelIcon />
                 </TimelineDot>
                 <TimelineConnector />
               </TimelineSeparator>
               <TimelineContent>
                 <Paper elevation={3} className={classes.paper}>
                   <Typography variant="h6" component="h1">
-                    Feb/2020
+                    After start my carer in development area
                   </Typography>
                   <Typography>
-                    Finished Bootcamp in web development at Lighthouse Labs
+                    Previously, I consolidated my 5 years of experience in law
+                    and in different types of companies, which allowed me to
+                    understand the complexity and dynamics of the corporate
+                    environment, develop personal skills and generate results
+                    for the teams in which I participated.
                   </Typography>
                 </Paper>
               </TimelineContent>
             </TimelineItem>
+
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot color="primary" variant="outlined">
+                  <LaptopMacIcon />
+                </TimelineDot>
+                <TimelineConnector className={classes.secondaryTail} />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography variant="h6" component="h1">
+                    July/2019
+                  </Typography>
+                  <Typography>
+                    July was where I started my career transition. I started an
+                    online Java script course, where I started to understand
+                    basic and in-depth concept of the language.
+                  </Typography>
+                </Paper>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot color="primary" variant="outlined">
+                  <SchoolIcon />
+                </TimelineDot>
+                <TimelineConnector className={classes.secondaryTail} />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography variant="h6" component="h1">
+                    Nov-Feb/2020
+                  </Typography>
+                  <Typography>
+                    I Finished Bootcamp in web development at Lighthouse Labs
+                  </Typography>
+                </Paper>
+              </TimelineContent>
+            </TimelineItem>
+
             <TimelineItem>
               <TimelineSeparator>
                 <TimelineDot color="primary">
@@ -101,43 +148,124 @@ const About = () => {
               <TimelineContent>
                 <Paper elevation={3} className={classes.paper}>
                   <Typography variant="h6" component="h1">
-                    Code
+                    March
                   </Typography>
                   <Typography>
-                    I developed the booklist is as a platform in which you can
-                    search for books and save them to read later.
+                    I developed a project called booklist where you can search
+                    for books and save them for later reading. It was a great
+                    way to start putting into practice what I had learned at
+                    Lighthouse
                   </Typography>
                 </Paper>
               </TimelineContent>
             </TimelineItem>
+
             <TimelineItem>
               <TimelineSeparator>
                 <TimelineDot color="primary" variant="outlined">
-                  <HotelIcon />
+                  <LaptopMacIcon />
                 </TimelineDot>
                 <TimelineConnector className={classes.secondaryTail} />
               </TimelineSeparator>
               <TimelineContent>
                 <Paper elevation={3} className={classes.paper}>
                   <Typography variant="h6" component="h1">
-                    Sleep
+                    April
                   </Typography>
-                  <Typography>Because you need rest</Typography>
+                  <Typography>
+                    After that I did a voluntary work that was to develop a
+                    website for a hackaton. It was a great experience. In the
+                    same month I started taking a basic python course. In the
+                    bootcamp I didn’t learn python, so as I’m always looking for
+                    new knowledge so I decided to do it and it was really cool.
+                  </Typography>
                 </Paper>
               </TimelineContent>
             </TimelineItem>
+
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot color="primary">
+                  <CodeIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography variant="h6" component="h1">
+                    May/July
+                  </Typography>
+                  <Typography>
+                    I Volunteering for Bloom Venture where I had to develop
+                    projects for the company's partners in the Clojure Script
+                    programming language. This volunteering gave me the
+                    experience to understand how to run a company's processes in
+                    the technology area and also to work as a pair programmer.
+                  </Typography>
+                </Paper>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot color="primary">
+                  <LaptopMacIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography variant="h6" component="h1">
+                    August
+                  </Typography>
+                  <Typography>
+                    I developed another project to continue studying react and
+                    javascript. Then I participated in a blended learning
+                    program, where I had the opportunity to combine valuable
+                    work experience and helped me to develop personal skills,
+                    such as communication, collaboration and problem solving.
+                    And also in the web development part. My project is
+                    available in the part of projects with the name Glaukopis
+                    wisdon.
+                  </Typography>
+                </Paper>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot color="primary">
+                  <CodeIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography variant="h6" component="h1">
+                    Setember
+                  </Typography>
+                  <Typography>
+                    I continued studying by attending conferences to get
+                    inspired, I participated in a mentoring group to continue
+                    growing in the development career that I love so much and I
+                    started developed this portfolio.
+                  </Typography>
+                </Paper>
+              </TimelineContent>
+            </TimelineItem>
+
             <TimelineItem>
               <TimelineSeparator>
                 <TimelineDot color="secondary">
-                  <RepeatIcon />
+                  <CodeIcon />
                 </TimelineDot>
               </TimelineSeparator>
               <TimelineContent>
                 <Paper elevation={3} className={classes.paper}>
                   <Typography variant="h6" component="h1">
-                    Repeat
+                    October
                   </Typography>
-                  <Typography>Because this is the life you love!</Typography>
+                  <Typography></Typography>
                 </Paper>
               </TimelineContent>
             </TimelineItem>
